1399b30dec0b05d6867affd34cec2b3cec02dbf7
Habil Muhammad <habilaja72@gmail.com>
2025-05-31 09:14:47 +0000

    update

---
+++ new-project/GhibliCafe-2/js/login.js
@@ -0,0 +1,31 @@
+// Toggle Password Visibility
+const togglePassword = document.querySelector(".toggle-password");
+const passwordInput = document.getElementById("login-password");
+if (togglePassword && passwordInput) {
+  togglePassword.addEventListener("click", () => {
+    const type = passwordInput.type === "password" ? "text" : "password";
+    passwordInput.type = type;
+    togglePassword.classList.toggle("fa-eye-slash");
+  });
+}
+
+// Login Form
+const loginForm = document.querySelector(".login-form");
+if (loginForm) {
+  loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const email = this.elements.email.value;
+    const password = this.elements.password.value;
+
+    // validation
+    if (!email.includes("@")) {
+      alert("Please enter a valid email");
+      return;
+    }
+
+    if (password.length < 6) {
+      alert("Password must be at least 6 characters");
+      return;
+    }
+  });
+}
